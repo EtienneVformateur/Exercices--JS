7536f4e11665541da2fcfec2ab1c59247b27d05a
EtienneV <formation.etienne.re@gmail.com>
2023-01-12 15:04:04 +0400

    Exercice 4 : Done

--- Exercice 4 - SIDEBAR/index.js
+++ Exercice 4 - SIDEBAR/index.js
@@ -1,9 +1,21 @@
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
+
 // Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
-
+// let btnActive = false;
+btn.addEventListener("click", () => {
+  //Methode 1 :
+  //   if (btnActive == false) {
+  //     sidebar.classList.add("active");
+  //     btnActive = true;
+  //   } else {
+  //     sidebar.classList.remove("active");
+  //     btnActive = false;
+  //   }
+  // Methode 2:
+  sidebar.classList.toggle("active");
+});
 // Ranger la sidebar si on click sur le contenu principal
-
-// BONUS
-// Créer en css l'hamburger menu qui se transforme en croix
-
-// Aide
-// Priorités en CSS : id > classe > baliseHtml
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
